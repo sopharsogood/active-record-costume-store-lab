83c77110e2f16b5033937ebbc898f775976c3c3b
sophie <justanothersophie@gmail.com>
2021-08-17 19:06:27 -0400

    commit tree 32a4d7598245240337b5e347439b9fca0c18c68e

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,7 @@
 # Create your CostumeStore class here
+
+require_relative '../../config/environment.rb'
+
+class CostumeStore < ActiveRecord::Base
+
+end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,7 @@
 # Create your HauntedHouse class here
+
+require_relative '../../config/environment.rb'
+
+class HauntedHouse < ActiveRecord::Base
+
+end
